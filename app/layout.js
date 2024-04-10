4f6a17a5521208a4b91e84061c4ea373c78f01f5
Your Name <you@example.com>
2024-04-10 16:37:00 -0400

    user table added

--- app/layout.js
+++ app/layout.js
@@ -26,6 +26,9 @@ export default function RootLayout({ children }) {
                   <Link href="/about">About</Link>
                 </li>
                 <li>
+                  <Link href="/users">Table</Link>
+                </li>
+                <li>
                   <Link href="/contact">Contact</Link>
                 </li>
               </ul>
---
+++ app/users/page.js
@@ -0,0 +1,44 @@
+"use client";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
+
+export default function UserTable() {
+  return (
+    <Table aria-label="Example static collection table">
+      <TableHeader>
+        <TableColumn>NAME</TableColumn>
+        <TableColumn>ROLE</TableColumn>
+        <TableColumn>STATUS</TableColumn>
+      </TableHeader>
+      <TableBody>
+        <TableRow key="1" className="text-white">
+          <TableCell>Tony Reichert</TableCell>
+          <TableCell>CEO</TableCell>
+          <TableCell>Active</TableCell>
+        </TableRow>
+        <TableRow key="2" className="text-white">
+          <TableCell>Zoey Lang</TableCell>
+          <TableCell>Technical Lead</TableCell>
+          <TableCell>Paused</TableCell>
+        </TableRow>
+        <TableRow key="3" className="text-white">
+          <TableCell>Jane Fisher</TableCell>
+          <TableCell>Senior Developer</TableCell>
+          <TableCell>Active</TableCell>
+        </TableRow>
+        <TableRow key="4" className="text-white">
+          <TableCell>William Howard</TableCell>
+          <TableCell>Community Manager</TableCell>
+          <TableCell>Vacation</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+}
